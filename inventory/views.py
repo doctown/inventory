e6611c11f3ed56011e331f94140dcdfbfaf7ac99
David Ogor <doctown@yahoo.com>
2016-07-18 16:17:34 -0700

    Add routes

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('<p>In index view</p>')
 
-# Create your views here.
+def item_details(request, id):
+    return HttpResponse('<p>In item_detail view with id {0}'.format(id))
